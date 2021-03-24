ecab588fced8810760d53f071b4afbb7c3fe2542
dahmad <daniyal.aziz.ahmad@gmail.com>
2021-03-23 21:02:46 -0700

    Removed tutorial routes and added empty routes for the actual project

--- app.py
+++ app.py
@@ -21,41 +21,30 @@ else:
 def hello():
     return "Hello World!"
 
+@app.route("/api/v1/recipes/", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def recipes():
+    return
 
-@app.route("/users/<string:user_id>")
-def get_user(user_id):
-    resp = client.get_item(
-        TableName=USERS_TABLE,
-        Key={
-            'userId': { 'S': user_id }
-        }
-    )
-    item = resp.get('Item')
-    if not item:
-        return jsonify({'error': 'User does not exist'}), 404
+@app.route("/api/v1/ingredients/", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def ingredients():
+    return
 
-    return jsonify({
-        'userId': item.get('userId').get('S'),
-        'name': item.get('name').get('S')
-    })
+@app.route("/api/v1/stores/", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def stores():
+    return
 
+@app.route("/api/v1/inventory_checklists/", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def inventory_checklists():
+    return
 
-@app.route("/users", methods=["POST"])
-def create_user():
-    user_id = request.json.get('userId')
-    name = request.json.get('name')
-    if not user_id or not name:
-        return jsonify({'error': 'Please provide userId and name'}), 400
+@app.route("/api/v1/shopping_lists/", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def shopping_lists():
+    return
 
-    resp = client.put_item(
-        TableName=USERS_TABLE,
-        Item={
-            'userId': {'S': user_id },
-            'name': {'S': name }
-        }
-    )
+@app.route("/api/v1/cooking_workflows/", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def cooking_workflows():
+    return
 
-    return jsonify({
-        'userId': user_id,
-        'name': name
-    })
+@app.route("/api/v1/calendars/", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def calendars():
+    return
